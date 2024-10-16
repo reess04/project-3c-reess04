05eb78064f2d15a24c541977da37b414ef2b3619
reess04 <reess@oregonstate.edu>
2024-10-16 15:51:51 -0700

    Update num_guess.py

--- num_guess.py
+++ num_guess.py
@@ -2,13 +2,13 @@ def guess():
     target = int(input("Enter the integer for the player to guess:\n"))
     attempts = 0
     correct = False
-    guess = int(input("Enter your guess:\n"))
     while correct == False:
+        guess = int(input("Enter your guess:\n"))
         attempts += 1
         if guess > target:
-            guess = int(input("Too high - try again:"))
+            print("Too high - try again:")
         elif guess < target:
-            guess = int(input("Too low - try again:"))
+            print("Too low - try again:")
         else:
             correct = True
     if attempts == 1:
