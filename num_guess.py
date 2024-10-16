b9b322eba92772e2255e58490ed5258c95a36ad6
reess04 <reess@oregonstate.edu>
2024-10-16 15:44:09 -0700

    Create num_guess.py

---
+++ num_guess.py
@@ -0,0 +1,19 @@
+def guess():
+    target_number = int(input("Enter the integer for the player to guess:\n"))
+    attempts = 0
+    correct = False
+    while correct == False:
+        guess = int(input("Enter your guess:\n"))
+        attempts += 1
+        if guess > target_number:
+            print("Too high - try again:")
+        elif guess < target_number:
+            print("Too low - try again:")
+        else:
+            correct = True
+    if attempts == 1:
+        print("You guessed it in 1 try.")
+    else:
+        print(f"You guessed it in {attempts} tries.")
+if __name__ == "__main__":
+    guess()
